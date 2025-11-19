2284ad36ea7b39f4e115c4c52f43392f8c794474
Nhp125 <nguyenhuuphuc12052005@gmail.com>
2025-11-19 19:53:49 +0700

    Mô tả ngắn gọn về thay đổi

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom"; // dùng HashRouter thay BrowserRouter
 import App from "./App";
 import "./styles.css";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
